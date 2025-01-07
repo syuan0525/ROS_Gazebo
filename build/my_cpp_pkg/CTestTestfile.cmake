e482805588aaba6764908e4003b122c5c3556d2d
syuan0525 <ss14ro@gmail.com>
2025-01-07 16:50:07 +0800

    新增 .gitignore 並移除不必要的檔案

--- build/my_cpp_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/my_cpp_pkg
-# Build directory: /root/catkin_ws/build/my_cpp_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/workspace_1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/workspace_1
-# Build directory: /root/catkin_ws/build/workspace_1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
